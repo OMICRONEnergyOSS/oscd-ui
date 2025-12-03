ca8547bb03eee5a176eebe11bd424b2a08f88b12
Stephen Renwick <stephen.renwick@omicronenergy.com>
2025-12-03 14:23:11 +0100

    feat: add oscd-scl-icon to display SCL Icons

---
+++ scl-icon/oscd-scl-icon.ts
@@ -0,0 +1,10 @@
+/*
+ * GENERATED SOURCE FILE. DO NOT MODIFY.
+ * Modifications will be overwritten.
+ * To prevent this file from being overwritten, remove this comment entirely.
+ */
+
+import { OscdSclIcon } from './OscdSclIcon.js';
+
+customElements.define('oscd-scl-icon', OscdSclIcon);
+export { OscdSclIcon };
